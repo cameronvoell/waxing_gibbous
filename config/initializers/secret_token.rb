0ed8b8da1bdd10026c31afae1dc3b0904022245a
Cameron Voell <cameronvoell@gmail.com>
2011-10-09 00:25:50 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+WaxingGibbous::Application.config.secret_token = 'fbfa3739220fa81eaafdb8ca5af9ee8809f5b3c45e0bf73fe7a0fc91c82b5367f3175693f205110586d1d51f2748f7af3b3f21cad73a3a6852796767440f628f'
